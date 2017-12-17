5d50b1dcdfc5c18c4e919321f3cf1a28c37cb3fe
jared.bledsoe <jared.bledsoe@icloud.com>
2017-12-17 13:21:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express();
+.use((req, res) => res.sendFile(INDEX));
 
 const wss = new SocketServer({ server });
 
