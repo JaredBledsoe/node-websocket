8e7ceac6fde7cedf297a1aaed22f3af830da832a
jared.bledsoe <jared.bledsoe@icloud.com>
2017-12-17 13:24:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-.use((req, res) => res.sendFile(INDEX));
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
 
